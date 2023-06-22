9063b85c04245c961a1bf290d0bba7e8a1c23e8f
snoW <snowcastle@qq.com>
2023-06-22 21:39:43 +0800

    添加并配置了nacos，

--- gulimall-order/src/main/java/com/xue/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/xue/gulimall/order/GulimallOrderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.xue.gulimall.orer.dao")
+@MapperScan("com.xue.gulimall.order.dao")
 public class GulimallOrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(GulimallOrderApplication.class,args);
